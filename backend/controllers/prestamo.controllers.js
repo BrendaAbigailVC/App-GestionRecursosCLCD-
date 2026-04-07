fb98fad88cdc2e010125ed573471331fd350a53b
BrendaAbigailVC <109320578+BrendaAbigailVC@users.noreply.github.com>
2026-04-06 20:37:19 -0600

    Habilitar prestamos a empleados v3

--- backend/controllers/prestamo.controllers.js
+++ backend/controllers/prestamo.controllers.js
@@ -21,12 +21,26 @@ const getPrestamosActivos = async (req, res, next) => {
   try {
     const result = await pool.query(`
       SELECT 
-        p.*, 
-        a.matricula AS alumno_matricula, 
+        p.*,
+        CASE 
+          WHEN p.solicitante_tipo = 'ALUMNO' THEN a.matricula
+          ELSE e2.noeconomico
+        END AS solicitante_codigo,
+        CASE 
+          WHEN p.solicitante_tipo = 'ALUMNO' THEN CONCAT(a.nombre, ' ', a.apellidopaterno)
+          ELSE e2.nombre
+        END AS solicitante_nombre,
+        p.solicitante_tipo,
         e.nombre AS empleado_nombre
       FROM prestamo p
-      LEFT JOIN alumno a ON a.id = p.idalumno
-      LEFT JOIN empleado e ON e.id = p.idempleado
+      LEFT JOIN alumno a 
+        ON p.solicitante_id = a.id 
+        AND p.solicitante_tipo = 'ALUMNO'
+      LEFT JOIN empleado e2 
+        ON p.solicitante_id = e2.id 
+        AND p.solicitante_tipo = 'EMPLEADO'
+      LEFT JOIN empleado e 
+        ON e.id = p.idempleado
       WHERE p.estadoprestamo = 0
     `);
     res.json(result.rows);
